f5a2688740fe1c92d7da2373e08325533e0f5467
Bruno de Araujo Alves <me@baraus.dev>
2023-02-06 21:41:22 -0300

    Updates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ export function concatAPIUrl(url: string | undefined): string {
   return `${process.env.NEXT_PUBLIC_API_URL}${url ?? ''}`;
 }
 export function concatSSRUrl(url: string | undefined): string {
-  return `${process.env.NEXT_PUBLIC_SSR_URL}${url ?? ''}`;
+  return `${process.env.NEXT_PUBLIC_SSR_API_URL}${url ?? ''}`;
 }
 
 export function clamp(num: number, min: number, max: number): number {
