177859da2449b58c9cfe3550b84520cf7eb823d6
devbaraus <me@baraus.dev>
2023-03-04 21:05:45 -0300

    Update deploy

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   /* config options here */
   images: {
-    domains: [
-      'images.unsplash.com',
-      '127.0.0.1',
-      'devbaraus.200.17.57.29.nip.io',
-      '172.17.0.3',
-      '172.17.0.5',
-    ],
+    domains: ['images.unsplash.com', '127.0.0.1', 'api.netro.baraus.dev'],
   },
   async redirects() {
     return [
