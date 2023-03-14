2dd9f3f8395ef8941490a4911a0b06319adc0512
Bruno de Araujo Alves <me@baraus.dev>
2023-03-14 16:04:25 -0300

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,6 +61,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|auth|v1|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|v1|_next/static|_next/image|favicon.ico).*)',
   ],
 };
